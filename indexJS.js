f024a56865ccb88ec020f108b33dbd412a6fc923
YuriiLeo <yuriileontenko@gmail.com>
2022-06-21 15:47:15 +0300

    +

---
+++ indexJS.js
@@ -0,0 +1,140 @@
+// function greet(name) {
+//     return `Ласкаво просимо ${name}.`;
+//   }
+  
+//   // Викликаємо функцію greet і виводимо результат у консоль
+//   console.log(greet("Манго")); // Ласкаво просимо Манго.
+  
+//   // Виводимо функцію greet у консоль, не викликаючи її
+//   console.log(greet); // ƒ greet() { return `Ласкаво просимо ${name}.`; }
+
+// Колбек-функція
+
+// function greet(name) {
+//     console.log(`Ласкаво просимо ${name}.`);
+//   }
+  
+//   // Функція вищого порядку
+//   function registerGuest(name, callback) {
+//     console.log(`Реєструємо гостя ${name}.`);
+//     callback(name);
+//   }
+  
+//   registerGuest("Манго", greet);
+
+// function registerGuest(name, callback) {
+//     console.log(`Реєструємо гостя ${name}.`);
+//     callback(name);
+//   }
+  
+//   // Передаємо інлайн функцію greet у якості колбека
+//   registerGuest("Манго", function greet(name) {
+//     console.log(`Ласкаво просимо ${name}.`);
+//   });
+  
+//   // Передаємо інлайн функцію notify у якості колбека
+//   registerGuest("Полі", function notify(name) {
+//     console.log(`Шановний(а) ${name}, ваш номер буде готовий за 30 хвилин.`);
+//   });
+
+// function processCall(recipient, onAvailable, onNotAvailable) {
+//     // Імітуємо доступність абонента випадковим числом
+//     const isRecipientAvailable = Math.random() > 0.5;
+  
+//     if (!isRecipientAvailable) {
+//       onNotAvailable(recipient);
+//       return;
+//     }
+  
+//     onAvailable(recipient);
+//   }
+  
+//   function takeCall(name) {
+//     console.log(`З'єднуємо з ${name}, очікуйте...`);
+//     // Логіка прийняття дзвінка
+//   }
+  
+//   function activateAnsweringMachine(name) {
+//     console.log(`Абонент ${name} недоступний, залиште повідомлення.`);
+//     // Логіка активації автовідповідача
+//   }
+  
+//   function leaveHoloMessage(name) {
+//     console.log(`Абонент ${name} недоступний, записуємо голограму.`);
+//     // Логіка запису голограми
+//   }
+  
+//   processCall("Манго", takeCall, activateAnsweringMachine);
+//   processCall("Полі", takeCall, leaveHoloMessage);
+
+// const students = [
+//   { name: "Манго", courses: ["математика", "фізика"] },
+//   { name: "Полі", courses: ["інформатика", "математика"] },
+//   { name: "Ківі", courses: ["фізика", "біологія"] },
+// ];
+
+// const allCourses = students.flatMap(student => student.courses);
+
+// console.log(allCourses);
+
+// const uniqueCourses = allCourses.filter(
+//   (course, index, array) => array.indexOf(course) === index
+// );
+
+// console.log(uniqueCourses);
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Dragon breath", price: 780 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+//     console.log(this.potions.includes(newPotion));
+
+//     if (this.potions.includes(newPotion)) {
+//       return `Error! Potion ${newPotion} is already in your inventory!`;
+//     }
+
+//     this.potions.push(newPotion);
+//   },
+//   removePotion(potionName) {
+//     const potionIndex = this.potions.indexOf(potionName);
+
+//     if (potionIndex === -1) {
+//       return `Potion ${potionName} is not in inventory!`;
+//     }
+
+//     this.potions.splice(potionIndex, 1);
+//   },
+//   updatePotionName(oldName, newName) {
+//     const potionIndex = this.potions.indexOf(oldName);
+
+//     if (potionIndex === -1) {
+//       return `Potion ${oldName} is not in inventory!`;
+//     }
+
+//     this.potions.splice(potionIndex, 1, newName);
+//   },
+// };
+
+
+// const names = ['Ivan', 'Olga', 'Olga', 'Ann', 'Olga'];
+// function changeName (arr, name, newName) {
+//   const aroow = [];
+//   for (let i = 0; i < arr.length; i += 1){
+//     if (name === arr[i]) {
+//       arr[i] = newName;
+//       console.log(`Name changed on ${newName}`);
+//       continue;
+//     } console.log (`this user name ${arr[i]} ...`)
+    
+//   }
+ 
+// }
+// changeName (names, 'Olga', 'Anastasiya')
+
+
